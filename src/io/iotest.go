01fd762741c809d398a44ce4ad97b96f6ead7c35
yangfu <785409885@qq.com>
2017-09-04 20:35:51 +0800

    用例

---
+++ src/io/iotest.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	data, err := ReadFrom(strings.NewReader("from string..."), 12)
+	if err == nil {
+		fmt.Println(string(data))
+	}
+
+	file, err := os.Open("E:/app/Go_Myeclipse/GoWorkSpace/src/github.com/tiptok/OFAppTest/RecordLog.txt")
+	defer file.Close()
+}
+
+func ReadFrom(r io.Reader, num int) ([]byte, error) {
+	p := make([]byte, num)
+	n, err := r.Read(p)
+	if n > 0 {
+		return p[:n], nil
+	}
+	return p, err
+}
