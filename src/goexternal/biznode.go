6b56fe55217741209ae6c9ca9c787f09279a8d81
yangfu <785409885@qq.com>
2018-03-06 17:50:27 +0800

    添加分发节点 TcpBizNode

---
+++ src/goexternal/biznode.go
@@ -0,0 +1,69 @@
+package goexternal
+
+import (
+	"sync"
+
+	"github.com/tiptok/gotransfer/conn"
+)
+
+/*
+   链接交换节点
+*/
+type TcpBizNode struct {
+	/*分发地址+*/
+	Key string
+	/*源*/
+	Src *conn.Connector
+	/*目标*/
+	Dst   *conn.Connector
+	mutex *sync.RWMutex
+}
+
+//发送给 src
+func (node *TcpBizNode) SendToSrc(d conn.TcpData) bool {
+	if node.Src.IsConneted {
+		node.Src.SendChan <- d
+	} else {
+		return false
+	}
+	return true
+}
+
+//发送给 dst
+func (node *TcpBizNode) SendToDst(d conn.TcpData) bool {
+	if node.Dst.IsConneted {
+		node.Dst.SendChan <- d
+	} else {
+		return false
+	}
+	return true
+}
+
+func (node *TcpBizNode) GetKey() string {
+	return node.Key
+}
+
+//关闭释放
+func (node *TcpBizNode) Disposed() {
+	node.mutex.Lock()
+	node.Src.Close()
+	//node.Dst.Close()
+	node.mutex.Unlock()
+}
+
+func NewTcpBizNode(src *conn.Connector, dstIP string, dstPort int) *TcpBizNode {
+	var tcpClient conn.TcpClient
+	tcpClient.NewTcpClient(dstIP, dstPort, 500, 500)
+	handler := &TcpExClient{}
+	if !tcpClient.Start(handler) {
+		//client 启动失败
+	}
+	node := &TcpBizNode{
+		Key:   src.RemoteAddress,
+		Src:   src,
+		Dst:   tcpClient.Conn,
+		mutex: new(sync.RWMutex),
+	}
+	handler.BizNode = node
+	return node
+}
--- src/goexternal/tcpex.go
+++ src/goexternal/tcpex.go
@@ -1,8 +1,6 @@
 package goexternal
 
 import (
-	"sync"
-
 	"log"
 
 	"github.com/tiptok/gotransfer/comm"
@@ -37,73 +35,14 @@ func (srv *tcpExserver) OnClose(c *conn.Connector) {
 	}()
 	key := c.RemoteAddress
 	node, isEx := srv.BizManager.GetOk(key)
-	log.Println("OnClose:", key, " ManageExists:", isEx)
-	node.(*TcpBizNode).Disposed()
+	log.Println("OnClose:", key, " ManageExists:", isEx, node)
+	//node.(*TcpBizNode).Disposed()
+	if tcpnode, isNode := node.(*TcpBizNode); isNode {
+		tcpnode.Disposed() //异常
+	}
 	srv.BizManager.Delete(key)
 	_, isEx = srv.BizManager.GetOk(key)
 	log.Println("OnClose:", key, " ManageExists:", isEx)
-}
-
-/*
-   链接交换节点
-*/
-type TcpBizNode struct {
-	/*分发地址+*/
-	Key string
-	/*源*/
-	Src *conn.Connector
-	/*目标*/
-	Dst   *conn.Connector
-	mutex *sync.RWMutex
-}
-
-//发送给 src
-func (node *TcpBizNode) SendToSrc(d conn.TcpData) bool {
-	if node.Src.IsConneted {
-		node.Src.SendChan <- d
-	} else {
-		return false
-	}
-	return true
-}
-
-//发送给 dst
-func (node *TcpBizNode) SendToDst(d conn.TcpData) bool {
-	if node.Dst.IsConneted {
-		node.Dst.SendChan <- d
-	} else {
-		return false
-	}
-	return true
-}
-
-func (node *TcpBizNode) GetKey() string {
-	return node.Key
-}
-
-//关闭释放
-func (node *TcpBizNode) Disposed() {
-	node.mutex.Lock()
-	node.Src.Close()
-	//node.Dst.Close()
-	node.mutex.Unlock()
-}
-
-func NewTcpBizNode(src *conn.Connector, dstIP string, dstPort int) *TcpBizNode {
-	var tcpClient conn.TcpClient
-	tcpClient.NewTcpClient(dstIP, dstPort, 500, 500)
-	handler := &TcpExClient{}
-	if !tcpClient.Start(handler) {
-		//client 启动失败
-	}
-	node := &TcpBizNode{
-		Key:   src.RemoteAddress,
-		Src:   src,
-		Dst:   tcpClient.Conn,
-		mutex: new(sync.RWMutex),
-	}
-	handler.BizNode = node
-	return node
 }
 
 type TcpExClient struct {
