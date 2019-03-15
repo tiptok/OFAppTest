7f9c0f415d7dcae711a5d39eed24c569fa0e5e45
yangfu <785409885@qq.com>
2019-03-15 16:08:37 +0800

    原生sql 测试

--- src/gormT/goorm_test.go
+++ src/gormT/goorm_test.go
@@ -1,9 +1,9 @@
 package gormT
 
 import (
-	"testing"
-
+	"database/sql"
 	"fmt"
+	"testing"
 
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/jinzhu/gorm"
@@ -57,4 +57,98 @@ func TestMysqlQuery(t *testing.T) {
 	db.Table("bas_AlarmFlag").First(&alarmItem)
 	fmt.Println("AlarmFlag Item:", alarmItem)
 	t.Log("End")
+}
+
+/*测试原生sql 操作*/
+
+func mysql_instance() (*sql.DB, error) {
+	sdsn := "root:admin@tcp(47.254.82.16:3306)/TopDB?charset=utf8mb4"
+	return sql.Open("mysql", sdsn)
+}
+
+func Test_mysql_insert(t *testing.T) {
+	db, err := mysql_instance()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stmt, err := db.Prepare(`INSERT INTO bas_AlarmFlag(AlarmFlag,AlarmName)VALUES(?,?)`)
+	defer stmt.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ret, err := stmt.Exec(8192, "超速预警")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rowsAffected, err := ret.RowsAffected(); err == nil {
+		fmt.Println("RowsAffected:", rowsAffected)
+	}
+}
+
+func Test_mysql_update(t *testing.T) {
+	db, err := mysql_instance()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stmt, err := db.Prepare(`UPDATE bas_AlarmFlag SET AlarmName=? WHERE AlarmFlag=?`)
+	defer stmt.Close()
+	ret, err := stmt.Exec("超速预警1", 8192)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rowsAffected, err := ret.RowsAffected(); err == nil {
+		fmt.Println("RowsAffected:", rowsAffected)
+	}
+}
+
+func Test_mysql_delete(t *testing.T) {
+	db, err := mysql_instance()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stmt, err := db.Prepare(`DELETE FROM bas_AlarmFlag WHERE AlarmFlag=?`)
+	defer stmt.Close()
+	ret, err := stmt.Exec(8192)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if rowsAffected, err := ret.RowsAffected(); err == nil {
+		fmt.Println("RowsAffected:", rowsAffected)
+	}
+}
+
+func Test_mysql_select(t *testing.T) {
+	db, err := mysql_instance()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stmt, err := db.Prepare(`SELECT * FROM bas_AlarmFlag`)
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	columns, _ := rows.Columns()
+	fmt.Println("Columns:", columns)
+
+	scans := make([]interface{}, len(columns))
+	//valu
+	i := 0
+	for rows.Next() {
+		PaddingMapValue(scans)
+		err = rows.Scan(scans...)
+		fmt.Printf("Index:%d Values:%v \n", i, scans)
+		i++
+	}
+}
+
+func PaddingMapValue(m []interface{}) {
+	for i := 0; i < len(m); i++ {
+		var value interface{}
+		m[i] = &value
+	}
+}
+
+func PaddingMapValue(m []interface{}) {
+	for i := 0; i < len(m); i++ {
+		var value interface{}
+		m[i] = &value
+	}
 }
