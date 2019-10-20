3f7a0ae1421d9ee75e4eab8cd97d72b3180df674
yangfu <785409885@qq.com>
2019-10-20 22:13:35 +0800

    go lib io multi.go

--- src/golib/ioT/io_test.go
+++ src/golib/ioT/io_test.go
@@ -1,8 +1,10 @@
 package ioT
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
 	"testing"
@@ -25,4 +27,31 @@ func ReadFrom(r io.Reader, num int) ([]byte, error) {
 		return p[:n], nil
 	}
 	return p, err
+}
+
+func TestMultiRead(t *testing.T){
+	r1:=strings.NewReader("tip")
+	r2:=strings.NewReader("tok")
+	mr :=io.MultiReader(r1,r2)
+	var buf []byte = make([]byte,10)
+	rn,_:=mr.Read(buf) //只会读取到第一个reader
+	t.Log("Read:",string(buf),rn)
+	io.ReadFull(mr,buf[rn:])//io.ReadFull(mr,buf)
+	t.Log(string(buf))
+
+	allocs :=testing.AllocsPerRun(1000,func(){
+		io.Copy(os.Stdout,mr)
+	})
+	t.Log("allocs:",allocs)
+}
+
+func TestMultiWriter(t *testing.T){
+	r :=strings.NewReader("tiptok")
+	var buf1,buf2 bytes.Buffer
+	w :=io.MultiWriter(&buf1,&buf2)
+	if _,err :=io.Copy(w,r);err!=nil{ //同时写入两个writer
+		log.Fatal(err)
+	}
+	t.Log(buf1.String())
+	t.Log(buf2.String())
 }
